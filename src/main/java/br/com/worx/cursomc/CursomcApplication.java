5b5ef4e131bd709a3054c03af5bbf09cc0630940
Robas <pax.profundis@gmail.com>
2020-03-22 13:54:01 -0300

    Automatically populating db

--- src/main/java/br/com/worx/cursomc/CursomcApplication.java
+++ src/main/java/br/com/worx/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package br.com.worx.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import br.com.worx.cursomc.domain.Categoria;
+import br.com.worx.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
