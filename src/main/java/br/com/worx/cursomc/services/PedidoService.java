67394af7e98f73934b93371910974dcebac2bfd9
Robas <pax.profundis@gmail.com>
2020-04-04 13:11:21 -0300

    Endpoint /pedidos/{id} disponível

---
+++ src/main/java/br/com/worx/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package br.com.worx.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.worx.cursomc.domain.Pedido;
+import br.com.worx.cursomc.exceptions.ObjectNotFoundException;
+import br.com.worx.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
