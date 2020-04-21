906f31dd71198cbdef760a8681f691bf9fa14842
Robas <pax.profundis@gmail.com>
2020-04-21 15:07:30 -0300

    Customizing validation return

---
+++ src/main/java/br/com/worx/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,26 @@
+package br.com.worx.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> list = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timestamp) {
+		super(status, msg, timestamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return list;
+	}
+
+	public void addError(String fieldName, String mensagem) {
+		list.add(new FieldMessage(fieldName, mensagem));
+	}
+
+	
+
+	
+}
