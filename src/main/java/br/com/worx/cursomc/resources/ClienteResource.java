037a58225d9fe93b78a8f558f8f892c70421e885
Robas <pax.profundis@gmail.com>
2020-04-27 21:17:13 -0300

    Inserindo um novo cliente com POST

--- src/main/java/br/com/worx/cursomc/resources/ClienteResource.java
+++ src/main/java/br/com/worx/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package br.com.worx.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,10 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.worx.cursomc.domain.Cliente;
-import br.com.worx.cursomc.domain.Cliente;
 import br.com.worx.cursomc.dto.ClienteDTO;
+import br.com.worx.cursomc.dto.ClienteNewDTO;
 import br.com.worx.cursomc.services.ClienteService;
 
 @RestController
@@ -72,6 +74,15 @@ public class ClienteResource {
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 		
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDto(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/br/com/worx/cursomc/services/ClienteService.java
+++ src/main/java/br/com/worx/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package br.com.worx.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,10 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.worx.cursomc.domain.Cidade;
 import br.com.worx.cursomc.domain.Cliente;
-import br.com.worx.cursomc.domain.Cliente;
+import br.com.worx.cursomc.domain.Endereco;
+import br.com.worx.cursomc.domain.enums.TipoCliente;
 import br.com.worx.cursomc.dto.ClienteDTO;
+import br.com.worx.cursomc.dto.ClienteNewDTO;
 import br.com.worx.cursomc.repositories.ClienteRepository;
+import br.com.worx.cursomc.repositories.EnderecoRepository;
 import br.com.worx.cursomc.services.exceptions.DataIntegrityException;
 import br.com.worx.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -23,6 +29,9 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
+	@Autowired
+	private EnderecoRepository enderecoRepo;
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 
@@ -30,6 +39,14 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepo.saveAll(obj.getEnderecos());
+		return obj;
+	}
+	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
@@ -62,6 +79,22 @@ public class ClienteService {
 
 	public Cliente fromDto(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDto(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 }
